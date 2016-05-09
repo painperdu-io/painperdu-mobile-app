ac5214e5f1fe6553c9f44e02a56307324cd419bc
Julien Martins Da Costa <julien.martinsdacosta@gmail.com>
2016-05-09 14:53:34 +0200

    Ajout des fonts de l'application

--- src/App.vue
+++ src/App.vue
@@ -34,4 +34,64 @@ body {
   background: #ffffff;
   overflow: hidden;
 }
+
+@font-face {
+  font-family: 'Avenir-Book';
+  src: url('/static/fonts/Avenir-Book.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/Avenir-Book.woff') format('woff'),
+    url('/static/fonts/Avenir-Book.ttf')  format('truetype'),
+    url('/static/fonts/Avenir-Book.svg#Avenir-Book') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Baskerville-Bold';
+  src: url('/static/fonts/Baskerville-Bold.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/Baskerville-Bold.woff') format('woff'),
+    url('/static/fonts/Baskerville-Bold.ttf')  format('truetype'),
+    url('/static/fonts/Baskerville-Bold.svg#Baskerville-Bold') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Karla-Bold';
+  src: url('/static/fonts/Karla-Bold.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/Karla-Bold.woff') format('woff'),
+    url('/static/fonts/Karla-Bold.ttf')  format('truetype'),
+    url('/static/fonts/Karla-Bold.svg#Karla-Bold') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Karla-BoldItalic';
+  src: url('/static/fonts/Karla-BoldItalic.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/Karla-BoldItalic.woff') format('woff'),
+    url('/static/fonts/Karla-BoldItalic.ttf')  format('truetype'),
+    url('/static/fonts/Karla-BoldItalic.svg#Karla-BoldItalic') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Karla-Italic';
+  src: url('/static/fonts/Karla-Italic.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/Karla-Italic.woff') format('woff'),
+    url('/static/fonts/Karla-Italic.ttf')  format('truetype'),
+    url('/static/fonts/Karla-Italic.svg#Karla-Italic') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Karla-Regular';
+  src: url('/static/fonts/Karla-Regular.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/Karla-Regular.woff') format('woff'),
+    url('/static/fonts/Karla-Regular.ttf')  format('truetype'),
+    url('/static/fonts/Karla-Regular.svg#Karla-Regular') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
 </style>
