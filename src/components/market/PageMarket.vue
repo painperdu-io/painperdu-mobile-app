b358d5a23384741d871c0e780e27e3a285d89ee0
mbrunelliere <marionbrnllr@gmail.com>
2016-05-11 16:27:55 +0200

    Structure page market

--- src/components/market/PageMarket.vue
+++ src/components/market/PageMarket.vue
@@ -1,8 +1,34 @@
 <template>
   <div>
-    <div id="produit" class="button" v-link="{ path: '/product' }">fiche produit</div>
-    <div class="button" v-link="{ path: '/clan' }">fiche clan</div>
-    <div class="button" v-link="{ path: '/addMarket' }">Ajouter une place du marché</div>
+    <!-- Ajouter produit lien
+      <div id="produit" class="button" v-link="{ path: '/product' }">fiche produit</div>
+    -->
+
+    <div class="group-item clan-item" v-link="{ path: '/clan' }">
+      <img src="" />
+      <div class="group-name">Nom Clan</div>
+    </div>
+
+    <input type="search" placeholder="Nom du produit..." />
+    <div class="product-filters">
+      <button>Produits Bruts</button>
+      <button>Produits Préparés</button>
+    </div>
+
+    <div class="products-list">
+      <div class="product">
+        <div>
+          <img src="" />
+          <div class="product-quantity"></div>
+        </div>
+        <div class="product-name">Nom du produit</div>
+        <div class="product-status">Disponible maintenant</div>
+      </div>
+    </div>
+
+    <!-- Ajouter place du marché
+      <div class="button" v-link="{ path: '/addMarket' }">Ajouter une place du marché</div>
+    -->
   </div>
 </template>
 
@@ -10,8 +36,13 @@
 export default {}
 </script>
 
-<style>
-  .button {
-    color: red;
-  }
+<style lang='scss' scoped>
+@import './../../styles/variables.scss';
+.wrap {
+  text-align: center;
+}
+.products-list {
+  text-align: center;
+}
+
 </style>
