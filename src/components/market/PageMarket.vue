331af899658bee2517624354ffcad2e935e1ba04
mbrunelliere <marionbrnllr@gmail.com>
2016-05-12 21:46:34 +0200

    Page place du marche

--- src/components/market/PageMarket.vue
+++ src/components/market/PageMarket.vue
@@ -4,23 +4,35 @@
 
     <div class="group-item clan-item" v-link="{ path: '/clan' }">
       <img src="./../../../static/temp/clan.png" />
-      <div class="group-name">Ma résidence</div>
+      <div class="group-name">
+        Ma résidence
+        <svg viewBox="0 0 100 100" class="star-icon">
+          <use xlink:href="#app-icon-star"></use>
+        </svg>
+      </div>
     </div>
 
     <div class="products-wrapper">
-      <div>
+      <div class="search-wrapper">
+        <svg viewBox="0 0 100 100" class="search-icon search-icon-left">
+          <use xlink:href="#app-icon-search"></use>
+        </svg>
+        <svg viewBox="0 0 100 100" class="search-icon search-icon-right">
+          <use xlink:href="#app-icon-close"></use>
+        </svg>
+
         <input id="search-input" placeholder="Nom du produit..." v-model="research" />
       </div>
 
       <div class="product-filters">
-        <input type="radio" value="false" v-model="productType">
-        <label for="one" class="filter">Plats Préparés</label>
-        <input type="radio" value="true" v-model="productType">
-        <label for="two" class="filter">Produits Bruts</label>
+        <input type="radio" id="brut" class="filter"value="false" v-model="productType">
+        <label for="brut" >Plats Préparés</label>
+        <input type="radio" id="prepare" class="filter" value="true" v-model="productType">
+        <label for="prepare">Produits Bruts</label>
       </div>
 
       <div class="products-list">
-        <template v-for="product in products | filterBy research in 'name' | filterBy productType in 'brut'  ">
+        <template v-for="product in products | filterBy research in 'name' | filterBy productType in 'brut' | orderBy 'name' ">
           <div class="product" v-bind:class="{ 'inactive': !product.status }" v-link:"{ path: '/product/:id' }">
             <div class="wrapper">
               <svg viewBox="0 0 100 100" class="foods-icon">
@@ -100,16 +112,28 @@ export default {
   text-align: center;
 }
 .group-item {
-  margin: auto;
+  position: relative;
+  margin: (60px / 2) auto 0;
   text-align: center;
   img {
-    transform: scale(0.5);
+    width: (338px / 2);
   }
   .group-name {
+    position: absolute;
+    bottom: 25px;
+    left: 50%;
+    transform: translateX(-50%);
     font-size: (2.5em / 2);
     font-family: 'Karla-Bold';
     text-transform: uppercase;
     color: $color-text;
+  }
+  .star-icon {
+    fill: $color-red;
+    width: (50px/ 2);
+    position: absolute;
+    left: 95%;
+    top: -55%;
   }
 }
 .products-list {
@@ -150,6 +174,7 @@ export default {
       font-size: (3.6em / 2);
       font-family: 'IowanOldStyle-Roman';
       font-weight: 700;
+      margin: (20px / 2) auto;
     }
     .product-status {
       color: $color-text;
@@ -158,6 +183,12 @@ export default {
     }
     &.inactive {
       filter: grayscale(100%);
+      .product-quantity {
+        background-color: $color-inactive;
+      }
+      .product-status {
+        opacity: 0.5;
+      }
     }
   }
 }
@@ -166,31 +197,66 @@ export default {
   text-align: center;
   line-height: (80px / 2);
     .filter {
-      background-color: rgba(0,0,0,0);
-      height: (80px / 2);
-      border: 2px solid white;
-      border-radius: (80px / 4);
-      text-transform: uppercase;
-      padding: (20px / 2);
-      color: $color-text;
-      font-family: 'Karla-Bold';
-      font-size: (2.3em / 2);
-      &::checked {
-        background-color: white;
+      + label {
+        background-color: rgba(0,0,0,0);
+        height: (80px / 2);
+        border: 2px solid white;
+        border-radius: (80px / 4);
+        text-transform: uppercase;
+        padding: (20px / 2);
+        color: $color-text;
+        font-family: 'Karla-Bold';
+        font-size: (2.3em / 2);
+      }
+      &:first-child {
+        + label {
+          margin-right: (40px / 2);
+
+        }
+      }
+      &:checked {
+        + label {
+          background-color: white;
+        }
       }
     }
 }
 
-#search-input {
-  background-color: $color-green;
-  border: none;
-  color: white;
-  margin:(50px / 2) auto (36px/2) ;
-  width: (525px / 2);
-  padding: 0 (25px / 2);
-  border-radius: (80px / 4);
-  height: (80px / 2);
+.search-icon {
+  width: (60px / 2);
+  height: (60px / 2);
+  margin-top: (10px / 2);
+  fill: $color-white;
+  position: absolute;
 }
 
-::-webkit-input-placeholder { color: white;}
+.search-icon-left {
+  left: 15px;
+}
+.search-icon-right {
+  right: 20px;
+}
+
+.search-wrapper {
+  position: relative;
+  margin:(50px / 2) auto (36px/2) ;
+  width: (525px / 2);
+}
+#search-input {
+  background-color: $color-blue;
+  border: none;
+  color: white;
+  padding: 0 (25px / 2) 0 (100px / 2);
+  border-radius: (80px / 4);
+  height: (80px / 2);
+  width: 100%;
+  box-sizing: border-box;
+  font-style: italic;
+}
+
+input[type="radio"] {
+  display: none;
+}
+
+::-webkit-input-placeholder { font-style: italic; color: white;}
 </style>
