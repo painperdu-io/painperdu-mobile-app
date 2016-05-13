459b62ec2286b53e1b5b28e14694f2aea59f96c5
Julien Martins Da Costa <julien.martinsdacosta@gmail.com>
2016-05-13 17:22:20 +0200

    Ajout de nouveaux composants

---
+++ src/components/commons/ResidenceSlider.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="residence-slider-container">
+    <div class="residence-slider-item" v-link="{ path: '/market/details/members' }">
+      <img src="./../../../static/temp/clan.png" />
+      <div class="residence-slider-item-name">
+        Ma résidence
+        <svg viewBox="0 0 100 100" class="residence-slider-item-star-icon">
+          <use xlink:href="#app-icon-star"></use>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import './../../styles/variables.scss';
+
+.residence-slider-container {
+  position: relative;
+  margin: 30px auto 0;
+}
+
+  .residence-slider-item {
+    text-align: center;
+    img {
+      width: 169px;
+    }
+  }
+
+    .residence-slider-item-name {
+      position: absolute;
+      bottom: 24px;
+      left: 50%;
+      transform: translate3d(-50%, 0, 0);
+      font: 1.25em "Karla-Bold", sans-serif;
+      text-transform: uppercase;
+      color: $color-text;
+    }
+
+    .residence-slider-item-star-icon {
+      position: absolute;
+      left: 95%;
+      top: -47%;
+      width: 25px;
+      fill: $color-red;
+    }
+</style>
---
+++ src/components/commons/navigation/TabsNavigation.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="tabs-navigation-container">
+    <nav class="tabs-navigation-wrapper">
+      <template v-for="tab in tabs">
+        <div class="tabs-navigation-item" v-link="{ path: tab.path, activeClass: 'active' }">{{ tab.name }}</div>
+      </template>
+    </nav>
+    <div class="tabs-navigation-active"></div>
+  </div>
+</template>
+
+<script>
+import { tabsNavigation } from './../../../vuex/getters'
+
+export default {
+  vuex: {
+    getters: {
+      tabs: tabsNavigation,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './../../../styles/variables.scss';
+
+.tabs-navigation-container {
+  position: relative;
+  width: 100%;
+  height: 60px;
+  background: $color-white;
+  z-index: 400;
+  overflow: hidden;
+}
+
+  .tabs-navigation-active {
+    position: absolute;
+    bottom: 0;
+    left: 33%;
+    height: 3px;
+    width: 33%;
+    background: $color-red;
+  }
+
+  .tabs-navigation-wrapper {
+    display: flex;
+    flex-flow: row wrap;
+    height: 55px;
+    width: 100%;
+  }
+
+    .tabs-navigation-item {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      flex: 1 33%;
+      font: 1.3em 'Karla-Regular', sans-serif;
+      color: $color-text;
+      text-align: center;
+      text-transform: uppercase;
+      text-decoration: none;
+      border-right: 1px solid $color-gray;
+
+      &:last-child {
+        border-right: none;
+      }
+
+      &.active {
+        font-weight: bold;
+        color: $color-red;
+      }
+    }
+</style>
