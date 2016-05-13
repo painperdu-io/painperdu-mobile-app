9df3a694fd9924e01fe93ef71a44baf25958eb9f
Julien Martins Da Costa <julien.martinsdacosta@gmail.com>
2016-05-13 11:01:58 +0200

    Nouveau loader

--- src/components/splash/Splash.vue
+++ src/components/splash/Splash.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="splash-container">
+    <div class="logo">
+      <svg viewBox="0 0 1024 768">
+        <use xlink:href="#logo-icon"></use>
+      </svg>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +13,22 @@ import router from '../../router'
 
 export default {
   ready () {
-    setTimeout(() => router.go({ name: 'Market' }), 2000)
+    setTimeout(() => router.go({ name: 'Market' }), 1000);
   },
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.splash-container {
+  position: relative;
+  width: 100vw;
+  height: calc(100vh - 160px);
+}
+
+.logo {
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  transform: translate3d(0, -50%, 0);
+}
+</style>
