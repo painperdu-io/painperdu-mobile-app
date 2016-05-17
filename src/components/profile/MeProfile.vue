514c1ae6ffb831fbbb0f6000f04c12b2c7b28e31
Julien Martins Da Costa <julien.martinsdacosta@gmail.com>
2016-05-17 19:42:12 +0200

    Mise à jour de la vue profile

--- src/components/profile/MeProfile.vue
+++ src/components/profile/MeProfile.vue
@@ -3,16 +3,23 @@
     <tabs-navigation route="UserProfile"></tabs-navigation>
     <div class="me-profile-infos-wrapper">
       <profile></profile>
+      <div class="me-profile-btn-public">Voir le profil public</div>
+    </div>
+    <div class="me-profile-accomplices-wrapper">
+      <h3 class="me-profile-accomplices-title">Mes compères</h3>
+      <accomplices-list></accomplices-list>
     </div>
   </div>
 </template>
 
 <script>
 import TabsNavigation from './../commons/navigation/TabsNavigation'
+import AccomplicesList from './../commons/AccomplicesList'
 import Profile from './../commons/Profile'
 
 export default {
   components: {
+    AccomplicesList,
     TabsNavigation,
     Profile,
   },
@@ -25,6 +32,27 @@ export default {
 .me-profile-container {}
 
   .me-profile-infos-wrapper {
-    margin-top: 20px;
+    margin: 20px 0 10px;
   }
+
+    .me-profile-btn-public {
+      margin: 0 auto;
+      width: 170px;
+      padding: 18px 0;
+      border-radius: 25px;
+      background-color: $color-beige;
+      font: 1.3em 'Karla-Bold', sans-serif;
+      color: $color-text;
+      text-transform: uppercase;
+      text-align: center;
+    }
+
+  .me-profile-accomplices-wrapper {
+    padding: 10px 30px 20px;
+  }
+
+    .me-profile-accomplices-title {
+      font: 1.3em 'Karla-Italic', sans-serif;
+      color: $color-text;
+    }
 </style>
