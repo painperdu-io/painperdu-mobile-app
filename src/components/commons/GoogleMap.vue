746c8b29a22926bb6ed5997b19185e6d28ce4f60
mbrunelliere <marionbrnllr@gmail.com>
2016-05-27 15:10:41 +0200

    google map style @todo// marker icon : image path

--- src/components/commons/GoogleMap.vue
+++ src/components/commons/GoogleMap.vue
@@ -1,35 +1,82 @@
 <template>
   <div class="google-map-container">
     <map
+      :map-type-id.sync="mapType"
       :center.sync="center"
       :zoom.sync="zoom"
-      :options="options">
+      :options="{ disableDefaultUI: true, styles: mapStyles,}"
+      >
       <marker
         v-for="m in markers"
-        :position.sync="m.position"></marker>
+        :icon.sync="markersIcon"
+        :position.sync="m.position"
+      >
+      </marker>
+      <circle
+        :bounds.sync="circleBounds"
+        :center.sync="center"
+        :radius.sync="circleRadius"
+        :options="circleOptions" >
+      </circle>
+
     </map>
     <div class="google-map-container-bg"></div>
   </div>
 </template>
 
 <script>
-import { load, Map, Marker } from 'vue-google-maps'
+import { load, Map, Marker, Circle } from 'vue-google-maps'
 load({ key: 'AIzaSyAyKy5Ep1QK1LekN9GJHu-uHGbUxQL7sfI' })
 
 export default {
   components: {
     Map,
     Marker,
+    Circle,
   },
   data () {
     return {
       zoom: 16,
-      options: {
-        disableDefaultUI: true,
-      },
       center : { lat: 48.8538302, lng: 2.2982161 },
       markers: [{ 'position': { lat: 48.8538302, lng: 2.2982161 } }],
+      /*markersIcon : {
+        url: '/static/img/stats-value.png',
+        size: { width: 40, height: 30 },
+        origin: { x: 0, y: 0},
+        anchor: { x: 0, y: 30},
+      },*/
+      mapStyle: 'default',
+      circleBounds: {},
+      circleRadius: 100,
+      circleOptions: {
+        editable: false,
+        strokeColor: '#25dec9',
+        strokeOpacity: 0.3,
+        strokeWeight: 2,
+        fillColor: '#25dec9',
+        fillOpacity: 0.3,
+      }
     };
+  },
+  computed: {
+    mapStyles () {
+      switch(this.mapStyle) {
+        default:
+        return [
+          {"featureType":"landscape","elementType":"all","stylers":[{"color":"#fcf9fc"}]},
+          {"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#e6e6e6"}]},
+          {"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#FFFFFF"}]},
+          {"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c7fcf7"}]},
+          {"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#dedede"}]},
+          {"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#4fbdb2"}]},
+          {"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"},{"hue":"#70ebdb"}]},
+          {"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},
+          {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#b8b8b8"}]},
+          {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#b8b8b8"}]},
+          {"featureType":"transit.station","elementType":"labels.icon","stylers":[{"visibility":"on"},{"hue":"#253b42"}]}
+        ];
+      }
+    }
   },
 };
 </script>
@@ -47,6 +94,5 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.4);
   }
 </style>
