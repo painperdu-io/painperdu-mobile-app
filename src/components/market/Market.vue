e4ddf8f80edbff6f6c00b8a070e2ae3754805fcf
mbrunelliere <marionbrnllr@gmail.com>
2016-06-05 17:41:22 +0200

    Market

--- src/components/market/Market.vue
+++ src/components/market/Market.vue
@@ -1,7 +1,21 @@
 <template>
+  <div class="markets-container">
 
-    <div class="market-slider">
-      <div class="market-container">
+    <div class="markets-slider">
+      <div class="controls">
+        <div class="previous" v-on:click="moveLeft">
+          <svg viewBox="0 0 50 50" class="controls-icon  controls-icon-previous">
+            <use xlink:href="#header-icon-next"></use>
+          </svg>
+        </div>
+        <div class="next" v-on:click="moveRight">
+          <svg viewBox="0 0 50 50" class="controls-icon  controls-icon-next">
+            <use xlink:href="#header-icon-next"></use>
+          </svg>
+        </div>
+      </div>
+
+      <div class="market-container market-1">
 
         <residence-slider :data="market"></residence-slider>
 
@@ -18,28 +32,32 @@
           </div>
 
 
-        <div class="market-products-filters">
-          <input id="brut" v-on:click="selectType" class="filter" type="checkbox" value="false" v-model="type">
-          <label for="brut" >Plats Préparés</label>
-          <input id="prepare" v-on:click="selectType" class="filter" type="checkbox" value="true" v-model="type">
-          <label for="prepare">Produits Bruts</label>
+          <div class="market-products-filters">
+            <input id="brut" class="filter" v-on:click="selectType" type="checkbox" value="false" v-model="type">
+            <label for="brut" >Plats Préparés</label>
+            <input id="prepare" class="filter" v-on:click="selectType" type="checkbox" value="true" v-model="type">
+            <label for="prepare">Produits Bruts</label>
+          </div>
+
+          <products-list
+            :products="market.products"
+            :type="type"
+            :search="search">
+          </producst-list>
         </div>
       </div>
 
+      <div class="market-container">
+        <div>
+          <img src="" />
+          Se rallier à une nouvelle place du marché
+        </div>
+      </div>
+
+
     </div>
 
-    <div v-link="{ path: '/market/add' }">
-      <img src="" />
-      Se rallier à une nouvelle place du marché
-    </div>
-
-    <div class="controls">
-      <div class="previous"></div>
-      <div class="next"></div>
-    </div>
   </div>
-
-
 </template>
 
 <script>
@@ -51,9 +69,9 @@ export default {
     ProductsList,
     ResidenceSlider,
   },
-  data() {
+  /*data() {
     return { market: { name: 'Nom de la résidence', products: {} }};
-  }
+  },
   ready() {
     this.$http({ url: 'products', method: 'GET' })
       .then((response) => {
@@ -63,11 +81,10 @@ export default {
         // This is certainly not a good way to handle errors
         console.log(err);
       });
-  }
+  },*/
 
-      //console.log(this.test);
-      //return test;
-    /*return {
+  data() {
+    return {
       market: {
         name: 'NOM RESIDENCE',
         products: [
@@ -115,7 +132,32 @@ export default {
           },
         ]
       }
-    }*/
+    }
+  },
+  methods : {
+    selectType: (event) => {
+      var el = event.target;
+      if(el.classList.contains('checked')) {
+        el.classList.remove('checked');
+      }
+      else {
+        el.classList.add('checked');
+        el.type.push(el.value);
+      }
+      event.preventDefault()
+    },
+    moveLeft: (event) => {
+      var slides = document.getElementsByClassName('market-container');
+      for (var i = 0; i < slides.length; i++) {
+          slides[i].style.transform = 'translate3D(100%, 0, 0)';
+      }
+    },
+    moveRight: (event) => {
+      var slides = document.getElementsByClassName('market-container');
+      for (var i = 0; i < slides.length; i++) {
+          slides[i].style.transform = 'translate3D(-100%, 0, 0)';
+      }
+    }
   }
 };
 </script>
@@ -123,7 +165,7 @@ export default {
 <style lang="scss" scoped>
 @import './../../styles/variables.scss';
 
-.market-container {}
+.markets-container {}
 
   .market-products-wrapper {
     text-align: center;
@@ -196,7 +238,48 @@ export default {
           }
         }
 
-
       }
     }
+
+    .controls {
+      position: absolute;
+      top: 65px;
+      width: 100%;
+      .previous,
+      .next {
+        width: 25px;
+        height: 25px;
+        border-radius: 50%;
+        background-color: $color-green-lite;
+        fill: $color-white;
+      }
+
+      .previous {
+        position: absolute;
+        left: 8%;
+      }
+
+      .next {
+        position: absolute;
+        right: 8%;
+      }
+
+    }
+
+    .markets-slider {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      overflow-x: auto;
+    }
+
+    .market-container {
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      min-width: 100%;
+      align-items: center;
+      transition: all 1s;
+    }
+
 </style>
