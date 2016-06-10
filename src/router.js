9c5dde85d79aad94e6121560e054d3eb486aff1f
mbrunelliere <marionbrnllr@gmail.com>
2016-06-10 20:11:53 +0200

    Router

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ Vue.use(VueTouch)
 global.currentUserId = '575302fc5dacbac32540268d';
 //global.currentUserId = '575302fc5dacbac32540268e';
 
-Vue.http.options.root = 'http://172.28.59.96:1337/api/v1';
+Vue.http.options.root = 'http://127.0.0.1:1337/api/v1';
 //Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
 Vue.http.options.xhr = { withCredentials: true }
 Vue.http.options.emulateHTTP = true;
