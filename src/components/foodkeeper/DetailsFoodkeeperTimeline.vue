962556e166b4a407aeb1d4762c140dbdf4890a17
Gobelins <gobelins@Mac-mini-de-Gobelins.local>
2016-06-13 19:28:30 +0200

    Foodkeeper

--- src/components/foodkeeper/DetailsFoodkeeperTimeline.vue
+++ src/components/foodkeeper/DetailsFoodkeeperTimeline.vue
@@ -16,8 +16,6 @@ export default {
 
 <style lang="scss" scoped>
 .details-foodkeeper-timeline-wrapper {
-  overflow-y: scroll;
-  max-height: 350px;
   img {
     width: 100%;
     overflow-y: scroll;
