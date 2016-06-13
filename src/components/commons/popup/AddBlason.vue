d2601b338484c453a5012f6d6d275d91ecd15a2e
Gobelins <gobelins@Mac-mini-de-Gobelins.local>
2016-06-13 07:50:38 +0200

    Popup add blason

--- src/components/commons/popup/AddBlason.vue
+++ src/components/commons/popup/AddBlason.vue
@@ -4,44 +4,44 @@
     <div class="popup-title-separator"></div>
       <div class="add-photo-popup-item-wrapper">
         <div class="add-photo-popup-item">
-          <div class="blason"></div>
+          <div class="blason {{shape}} {{color}}"></div>
         </div>
         <div class="add-photo-popup-item">
           <p>Sélectionnez votre forme<span class="underline"></span></p>
           <div class="add-photo-forms">
-            <input id="blason-1" class="form" type="radio" value="blason-1" v-model="form">
-            <label class="blason-1"  for="blason-1" ></label>
-            <input id="blason-2" class="form" type="radio" value="blason-2" v-model="form">
-            <label class="blason-2"  for="blason-2"></label>
-            <input id="blason-3" class="form" type="radio" value="blason-3" v-model="form">
-            <label class="blason-3"  for="blason-3" ></label>
-            <input id="blason-4" class="form" type="radio" value="blason-4" v-model="form">
-            <label class="blason-4"  for="blason-4" ></label>
+            <input id="blason-1" class="form" type="radio" value="blason-1" v-model="shape">
+            <label class="blason-1"  for="blason-1" ><img src="./../../../../static/img/shape-1.png" /></label>
+            <input id="blason-2" class="form" type="radio" value="blason-2" v-model="shape">
+            <label class="blason-2"  for="blason-2"><img src="./../../../../static/img/shape-2.png" /></label>
+            <input id="blason-3" class="form" type="radio" value="blason-3" v-model="shape">
+            <label class="blason-3"  for="blason-3" ><img src="./../../../../static/img/shape-3.png" /></label>
+            <input id="blason-4" class="form" type="radio" value="blason-4" v-model="shape">
+            <label class="blason-4"  for="blason-4" ><img src="./../../../../static/img/shape-4.png" /></label>
           </div>
         </div>
         <div class="add-photo-popup-item">
           <p>Choisissez votre illustration<span class="underline"></span></p>
           <div class="add-photo-illustrations">
             <input id="home" class="illustration" type="radio" value="home" v-model="illustration">
-            <label class="home"  for="home" ></label>
+            <label class="home"  for="home" ><img src="./../../../../static/img/illustration-1.png" /></label>
             <input id="school" class="illustration" type="radio" value="school" v-model="illustration">
-            <label class="school"  for="school"></label>
+            <label class="school"  for="school"><img src="./../../../../static/img/illustration-2.png" /></label>
             <input id="job" class="illustration" type="radio" value="job" v-model="illustration">
-            <label class="job"  for="job" ></label>
+            <label class="job"  for="job" ><img src="./../../../../static/img/illustration-3.png" /></label>
           </div>
         </div>
         <div class="add-photo-popup-item">
           <p>Choisissez votre couleur<span class="underline"></span></p>
           <div class="add-photo-colors">
-            <input id="green" class="color" type="radio" value="green" v-model="color">
+            <input id="green" class="color" type="radio" value="color-1" v-model="color">
             <label class="green"  for="green" ></label>
-            <input id="red" class="color" type="radio" value="red" v-model="color">
+            <input id="red" class="color" type="radio" value="color-2" v-model="color">
             <label class="red"  for="red"></label>
-            <input id="beige" class="color" type="radio" value="beige" v-model="color">
+            <input id="beige" class="color" type="radio" value="color-3" v-model="color">
             <label class="beige"  for="beige" ></label>
-            <input id="gray" class="color" type="radio" value="gray" v-model="color">
+            <input id="gray" class="color" type="radio" value="color-4" v-model="color">
             <label class="gray"  for="gray"></label>
-            <input id="sepia" class="color" type="radio" value="sepia" v-model="color">
+            <input id="sepia" class="color" type="radio" value="color-5" v-model="color">
             <label class="sepia"  for="sepia"></label>
           </div>
         </div>
@@ -138,6 +138,26 @@ export default {};
       width: 60px;
       height: 60px;
       background-color: $color-gray;
+      &.blason-1 {
+        mask-image: url('/static/img/blason-1.png');
+        -webkit-mask-image: url('/static/img/blason-1.png');
+      }
+      &.blason-2 {
+        mask-image: url('/static/img/blason-2.png');
+        -webkit-mask-image: url('/static/img/blason-2.png');
+      }
+      &.blason-3 {
+        mask-image: url('/static/img/blason-3.png');
+        -webkit-mask-image: url('/static/img/blason-3.png');
+      }
+      &.blason-4 {
+        mask-image: url('/static/img/blason-4.png');
+        -webkit-mask-image: url('/static/img/blason-4.png');
+      }
+
+      img {
+        width: 100%;
+      }
     }
   }
 
@@ -147,6 +167,9 @@ export default {};
       width: 40px;
       height: 40px;
       background-color: $color-gray;
+      img {
+        width: 100%;
+      }
     }
   }
 
